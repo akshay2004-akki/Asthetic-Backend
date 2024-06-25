bb74d22aa430d5c3d1dd840d65452b42fcbd9ffb
Akshay anand <akshay1622004@gmail.com>
2024-06-25 11:41:29 +0530

    removed unnecessary files

--- src/models/schedule.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from 'mongoose';
-
-const scheduleSchema = new Schema({
-    doctor: { type: Schema.Types.ObjectId, ref: 'Doctor', required: true },
-    day: { type: String, required: true },
-    timeSlots: [{ startTime: String, endTime: String, booked: Boolean }],
-}, { timestamps: true });
-
-export const Schedule = mongoose.model('Schedule', scheduleSchema);
