c2916d74e10b25a2c71538466436e0b5f57f557a
Akshay anand <akshay1622004@gmail.com>
2024-06-27 09:00:58 +0530

    add admin middleware

--- src/middlewares/admin.middleware.js
+++ src/middlewares/admin.middleware.js
@@ -1,9 +1,51 @@
 import { ApiError } from "../utils/ApiError.js";
+import asyncHandler from "../utils/asyncHandler.js";
 
- export const verifyAdmin = (req,res,next)=>{
-    const role = req.user.role;
-    if(!req.user || !role.toLocaleLowerCase()==='admin'){
-        throw new ApiError(500,"Access Denied, Only admins")
+export const isAdminAuthenticated = asyncHandler(async(req, res, next)=>{
+    const token = req.cookies.adminToken
+
+    if (!token) {
+        throw new ApiError(401, "Unauthorized Access!");
     }
-    next()
-}
+
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    req.user = await User.findById(decoded._id);
+    if (req.user.role !== "Admin") {
+        throw new ApiError(403, `${req.user.role} not authorized for this resource!`)
+    }
+    next();
+
+})
+
+export const isPatientAuthenticated = asyncHandler(async (req, res, next) => {
+    // Get token from request cookies
+    const token = req.cookies.patientToken;
+
+    // Verify token
+    if (!token) {
+        throw new ApiError(401, "Unauthorized Access!");
+    }
+
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    req.user = await User.findById(decoded._id);
+    if (req.user.role !== "Patient") {
+        throw new ApiError(403, `${req.user.role} not authorized for this resource!`)
+    }
+    next();
+});
+
+export const isDoctorAuthenticated = asyncHandler(async(req,res,next)=>{
+    const token = req.cookies.doctorToken
+
+    if(!token){
+        throw new ApiError(400,"Unauthorized Access")
+    }
+
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    req.user = await User.findById(decoded._id)
+
+    if (req.user.role !== "Doctor") {
+        throw new ApiError(403, `${req.user.role} not authorized for this resource!`)
+    }
+    next();
+})
