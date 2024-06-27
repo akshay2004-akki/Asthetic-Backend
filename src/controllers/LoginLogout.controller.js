c714634b3c5b6a0da32b978d32227709f70ceac5
Akshay anand <akshay1622004@gmail.com>
2024-06-27 09:54:03 +0530

    add login logout controller

---
+++ src/controllers/LoginLogout.controller.js
@@ -0,0 +1,45 @@
+import asyncHandler from "../utilis/asyncHandler.js";
+import { ApiError } from "../utilis/ApiError.js";
+import { User } from "../models/user.model.js";
+import { Doctor } from "../models/doctor.model.js"
+import { generateToken } from "../utilis/jwtToken.js";
+
+export const login = asyncHandler(async(req,res)=>{
+    const {email, password, role} = req.body
+    
+    if(!email || !password || !role) {
+        throw new ApiError("Please Fill Full Form!", 400);
+    }
+
+    let user
+    if(role==="Patient" || role==="Admin"){
+        user = await User.findOne({email}).select("+password") 
+    }else if(role==="Doctor"){
+        user = await Doctor.findOne({email}).select("+password")
+    }
+
+    if(!user){
+        throw new ApiError(400, `User with ${role} role not found`);
+    }
+
+    const isPasswordCorrect = await user.isPasswordCorrect(password)
+    if(!isPasswordCorrect){
+        throw new ApiError("Invalid email or password", 400);
+    }
+    generateToken(user,"User LoggedIn Successfully",200,res)
+})
+
+export const logoutAdmin = asyncHandler(async (req, res) => {
+    res
+        .status(200)
+        .cookie("adminToken", "", {
+            expires: new Date(Date.now()),
+            httpOnly: true,
+            secure: true,
+            sameSite: "None"
+        })
+        .json({
+            success: true,
+            message: "Admin logged out Successfully"
+        });
+})
